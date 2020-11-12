70284753d3bd2958c56cf4cacadc4a6e3c52b6a0
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-12 17:44:05 +0100

    Esercizio 2, utilizzo di una funzione anonima con evento click

--- Esercizio 2/js/script.js
+++ Esercizio 2/js/script.js
@@ -22,18 +22,25 @@ while ((isNaN(numeroDue)) || (numeroDue < 0) || (numeroDue > (myArray.length - 1
   numeroDue = parseInt(prompt("Per favore,inserisci un numero tra 0 e " + (myArray.length - 1)));
 }
 
-if (numeroDue < numeroUno) { // caso limite n.2
-  // ciclo for dove pusho gli elementi dell'array scelti dall'utente nel caso l'utente inserisca il secondo numero minore del primo
-  for (let i = numeroDue; i <= numeroUno; i++) {
-    arrayUtente.push(myArray[i])
-  } // fine ciclo for
-} else {
-  for (let i = numeroUno; i <= numeroDue; i++) { // nel caso l'utente inserisse il primo numero maggiore o uguale al secondo si verifica questo
-    arrayUtente.push(myArray[i])
-  } // fine ciclo for
-}// fine condizione if
 
+// --------------------- UPDATE 12/11/2020 utilizzo di una funzione anonima all'interno di un evento click.
 
-console.log(arrayUtente);
+document.getElementById('button').addEventListener('click',
+() => {
+
+  if (numeroDue < numeroUno) { // caso limite n.2
+    // ciclo for dove pusho gli elementi dell'array scelti dall'utente nel caso l'utente inserisca il secondo numero minore del primo
+    for (let i = numeroDue; i <= numeroUno; i++) {
+      arrayUtente.push(myArray[i])
+    } // fine ciclo for
+  } else {
+    for (let i = numeroUno; i <= numeroDue; i++) { // nel caso l'utente inserisse il primo numero maggiore o uguale al secondo si verifica questo
+      arrayUtente.push(myArray[i])
+    } // fine ciclo for
+  }// fine condizione if
+  document.getElementById('stampa').innerHTML = "Il risultato del tuo array è: " + arrayUtente;
+  console.log("Il risultato del tuo array è: ", arrayUtente);
+  return arrayUtente
+});
 
 //----------------------- FINE JSNACK 2 es6 ---------------------
